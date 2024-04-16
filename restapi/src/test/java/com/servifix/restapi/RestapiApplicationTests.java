c75abf0a810220345f3060db68c38937f2f06fc2
Luis Alberto Siancas Retegui <112042262+DahuX04@users.noreply.github.com>
2024-04-15 21:15:02 -0500

    initializing the project
    the base files of the project are uploaded

---
+++ restapi/src/test/java/com/servifix/restapi/RestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servifix.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
