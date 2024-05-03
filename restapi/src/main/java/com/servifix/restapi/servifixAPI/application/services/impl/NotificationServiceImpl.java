423881864455218900ae76210ad83e683135ae22
NotLT42 <u20211c667@upc.edu.pe>
2024-05-02 23:14:16 -0500

    Validations of Notifications completed
    All Notification validations were carried out and errors were corrected
    
    Co-Authored-By: Luis Alberto Siancas Retegui <112042262+DahuX04@users.noreply.github.com>
    Co-Authored-By: Lucía A. <91093842+luciaaliagat@users.noreply.github.com>
    Co-Authored-By: Jean Carlos Achamizo Huamani <102640048+Sorlac07@users.noreply.github.com>
    Co-Authored-By: Rodrigo Raymundo  <130820365+RodrigoRG24@users.noreply.github.com>

--- restapi/src/main/java/com/servifix/restapi/servifixAPI/application/services/impl/NotificationServiceImpl.java
+++ restapi/src/main/java/com/servifix/restapi/servifixAPI/application/services/impl/NotificationServiceImpl.java
@@ -4,15 +4,18 @@ import com.servifix.restapi.servifixAPI.application.dto.request.NotificationRequ
 import com.servifix.restapi.servifixAPI.application.dto.response.NotificationResponseDTO;
 import com.servifix.restapi.servifixAPI.application.dto.response.UserResponseDTO;
 import com.servifix.restapi.servifixAPI.application.services.NotificationService;
+import com.servifix.restapi.servifixAPI.domain.entities.Account;
 import com.servifix.restapi.servifixAPI.domain.entities.Notification;
 import com.servifix.restapi.servifixAPI.domain.entities.User;
 import com.servifix.restapi.servifixAPI.infraestructure.repositories.AccountRepository;
 import com.servifix.restapi.servifixAPI.infraestructure.repositories.NotificationRepository;
+import com.servifix.restapi.shared.exception.ValidationException;
 import com.servifix.restapi.shared.model.dto.response.ApiResponse;
 import com.servifix.restapi.shared.model.enums.Estatus;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 @Service
@@ -43,6 +46,7 @@ public class NotificationServiceImpl implements NotificationService {
     @Override
     public ApiResponse<NotificationResponseDTO> createNotification(NotificationRequestDTO notificationRequestDTO){
         var notification = modelMapper.map(notificationRequestDTO, Notification.class);
+        validateNotification(notification);
         notification.setAccount(accountRepository.getAccountById(notificationRequestDTO.getAccount()));
         notificationRepository.save(notification);
 
@@ -56,5 +60,23 @@ public class NotificationServiceImpl implements NotificationService {
         notificationRepository.deleteById(id);
         return new ApiResponse<>("Notification deleted successfully", Estatus.SUCCESS, null);
     }
+
+    private void validateNotification(Notification notification) {
+        if (existsNotificationByTitleByContentByAccount(notification.getTitle(), notification.getContent(), notification.getAccount().getId())) {
+            throw new ValidationException("Ya existe una notificación con el mismo título y contenido");
+        }
+        if (!isValidateDate(notification.getDate())) {
+            throw new ValidationException("La fecha de la notificación no puede ser anterior a la fecha actual");
+        }
+    }
+
+    private boolean isValidateDate(LocalDate date) {
+        return date.isAfter(LocalDate.now());
+    }
+
+    private boolean existsNotificationByTitleByContentByAccount(String title, String content, int account_id) {
+        return notificationRepository.existsByTitleAndContent(title, content) && notificationRepository.existsByAccount_Id(account_id);
+    }
+
 
 }
--- restapi/src/main/java/com/servifix/restapi/servifixAPI/infraestructure/repositories/NotificationRepository.java
+++ restapi/src/main/java/com/servifix/restapi/servifixAPI/infraestructure/repositories/NotificationRepository.java
@@ -1,9 +1,15 @@
 package com.servifix.restapi.servifixAPI.infraestructure.repositories;
 
+import com.servifix.restapi.servifixAPI.domain.entities.Account;
 import com.servifix.restapi.servifixAPI.domain.entities.Notification;
 import com.servifix.restapi.shared.model.dto.response.ApiResponse;
 import org.springframework.data.repository.CrudRepository;
 
 public interface NotificationRepository extends CrudRepository<Notification, Integer> {
+
+
+    boolean existsByTitleAndContent(String title, String content);
+
+    boolean existsByAccount_Id(int accountId);
 
 }
