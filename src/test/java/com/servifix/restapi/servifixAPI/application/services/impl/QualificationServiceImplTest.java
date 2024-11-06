a84e192b83b4cac4a7a1f308ef1461757c6fe3dd
Charlie <u202120841@upc.edu.pe>
2024-11-06 01:25:21 -0500

    fix: change conf for jenkins v.7

--- src/test/java/com/servifix/restapi/servifixAPI/application/services/impl/QualificationServiceImplTest.java
+++ src/test/java/com/servifix/restapi/servifixAPI/application/services/impl/QualificationServiceImplTest.java
@@ -15,7 +15,7 @@ class QualificationServiceImplTest {
     }
 
     @ParameterizedTest
-    @ValueSource(floats = { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f })
+    @ValueSource(floats = { 1.0f, 2.0f, 3.0f, 4.0f, 6.0f })
     public void testIsValidQualification(float quality){
         qualificationService = new QualificationServiceImpl();
         assertTrue(qualificationService.isValidQualification(quality));
